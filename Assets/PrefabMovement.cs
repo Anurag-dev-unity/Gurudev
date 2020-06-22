11b040e5c926e69a6391a9acfdcef0c415da3e56
Anurag-dev-unity <mir.anurag@gmail.com>
2020-06-22 21:42:41 +0530

    Version 1.0

---
+++ Assets/PrefabMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabMovement : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Vector3 endMarker;
+
+    // Movement speed in units per second.
+    private float speed = 0.1F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    void Start()
+    {
+        journeyLength = 0;
+    }
+
+    // Move to the target end position.
+    void Update()
+    {
+        if (journeyLength > 0)
+        {
+            // Distance moved equals elapsed time times speed..
+            float distCovered = (Time.time - startTime) * speed;
+
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / journeyLength;
+
+            // Set our position as a fraction of the distance between the markers.
+            transform.position = Vector3.Lerp(startMarker.position, endMarker, fractionOfJourney);
+        }
+    }
+    public void StartMove( Vector3 endPos)
+    {
+        startMarker = this.transform;
+        endMarker = endPos;
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, endMarker);
+        Debug.Log("Journey lenght is " + journeyLength);
+    }
+}
---
+++ Assets/Script/Wait.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+
+
+{
+    public float wait_time = 2.5f;
+
+    public GameObject show;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait_for_Intro());
+    }
+
+    IEnumerator Wait_for_Intro()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        show.SetActive(true);
+    }
+}
